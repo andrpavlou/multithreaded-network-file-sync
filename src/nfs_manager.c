8400d766461a9777206b9fceda77196075a8e6ee
andrpavlou <andreaspavlou2002@gmail.com>
2025-05-29 19:51:06 +0300

    added conf pairs into queue and threads handle the "add" cmd

--- src/nfs_manager.c
+++ src/nfs_manager.c
@@ -26,9 +26,7 @@ void handle_sigint(int sig){
 
 
 void *worker_thread(void *arg){
-    
     config_pairs* conf_pairs = (config_pairs*) arg;
-   
     int sock_source_read;
     if(establish_connection(&sock_source_read, conf_pairs->source_ip, conf_pairs->source_port)){
         perror("establish con");
@@ -264,7 +262,6 @@ int enqueue_add_cmd(const manager_command curr_cmd, sync_task_ts *queue_tasks, s
 
     char list_cmd_buff[BUFFSIZ];
     int list_len = snprintf(list_cmd_buff, sizeof(list_cmd_buff), "LIST %s\r\n", curr_cmd.source_dir);
-    // list_cmd_buff[list_len] = '\0'; ?? snprintf null term?
 
 
     // //========== Request List Command ========== //
@@ -275,10 +272,7 @@ int enqueue_add_cmd(const manager_command curr_cmd, sync_task_ts *queue_tasks, s
 
     char *list_reply_buff = NULL;
     read_list_response(sock_source_read, &list_reply_buff);
-    
-    // Locate ack and remove it
-    // char *ack_start = strstr(list_reply_buff, "\nACK\n");
-    // if(ack_start) ack_start[0] = '\0'; 
+
 
     char *file_buff[MAX_FILES];
     unsigned int total_src_files = get_files_from_list_response(list_reply_buff, file_buff);
@@ -324,10 +318,9 @@ int enqueue_add_cmd(const manager_command curr_cmd, sync_task_ts *queue_tasks, s
         sync_info_mem_store *inserted_node = add_sync_info(sync_info_head, source_full_path, target_full_path);
         if(inserted_node == NULL) {
             perror("sync info insert");    
-
+            
             free(new_task);
             close(sock_source_read);
-
             return 1;
         } 
 
@@ -392,6 +385,130 @@ int enqueue_cancel_cmd(const manager_command curr_cmd, sync_task_ts *queue_tasks
 }
 
 
+
+
+
+
+
+void *thread_exec_task(void *arg){
+    sync_task_ts *queue_tasks   = (sync_task_ts*)  arg;
+    sync_task *curr_task        = dequeue_task(queue_tasks);
+
+    if(curr_task->manager_cmd.op == ADD){
+        int sock_source_read;
+        if(establish_connection(&sock_source_read, curr_task->manager_cmd.source_ip, curr_task->manager_cmd.source_port)){
+            perror("establish con");
+            exit(1);
+        }   
+        int sock_target_push;
+        if(establish_connection(&sock_target_push, curr_task->manager_cmd.target_ip, curr_task->manager_cmd.target_port)){
+            perror("establish con");
+            exit(1);
+        }   
+        
+        char pull_buffer[BUFFSIZ];
+        char push_buffer[BUFFSIZ];
+        char pull_request_cmd_buff[BUFFSIZ];
+        
+        unsigned short first_push_write = 0;
+
+        memset(pull_request_cmd_buff, 0, sizeof(pull_request_cmd_buff));
+        int len_pull_req = snprintf(pull_request_cmd_buff, sizeof(pull_request_cmd_buff), 
+                            "PULL %s/%s\r\n",
+                            curr_task->manager_cmd.source_dir,
+                            curr_task->filename);
+        
+        // Request pull of given text
+        if(write_all(sock_source_read, pull_request_cmd_buff, len_pull_req) == -1){
+            perror("pull write");
+            return NULL;
+        }
+        
+        long file_size = 0;
+        if((file_size = get_file_size_of_host(sock_source_read)) == -1){
+            perror("get_file_size_of_host");
+            return NULL;
+        }
+        
+        
+        memset(pull_buffer, 0, sizeof(pull_buffer));
+        memset(push_buffer, 0, sizeof(push_buffer));
+        
+        ssize_t total_read_pull_req = 0;
+        // Read response of pull command 
+        while(total_read_pull_req <= file_size){
+            ssize_t request_bytes = (file_size - total_read_pull_req) < BUFFSIZ 
+                                    ? (file_size - total_read_pull_req) 
+                                    : BUFFSIZ - 1;
+
+
+            // Request_bytes = 0 -> just send push request with chunk size = 0 indicating the last push
+            if(!request_bytes){
+                int push_len = snprintf(push_buffer, sizeof(push_buffer), "PUSH %s/%s %d\r\n", 
+                                    curr_task->manager_cmd.target_dir,
+                                    curr_task->filename,
+                                    0);
+
+                if(write_all(sock_target_push, push_buffer, push_len) == -1){
+                    perror("push write");
+                }
+                break;
+            }
+            
+            ssize_t n_read;
+            if((n_read = read_all(sock_source_read, pull_buffer, request_bytes)) <= 0){
+                perror("\nread pull_buffer");
+                break;
+            }
+            
+
+            /*
+            * Send -1 batch size to create the file if it's the first write,
+            * otherwise send the bytes read from other host -> request_bytes
+            */
+            char push_header_buffer[BUFFSIZ];
+            int write_batch_bytes = first_push_write == 0 ? -1 : request_bytes; 
+            int push_header_len = snprintf(push_header_buffer, sizeof(push_header_buffer), "PUSH %s/%s %d\r\n", 
+                                curr_task->manager_cmd.target_dir,
+                                curr_task->filename,
+                                write_batch_bytes);
+
+            if(write_all(sock_target_push, push_header_buffer, push_header_len) == -1){
+                perror("write push");
+            }
+            if(!first_push_write){
+                push_header_len = snprintf(push_header_buffer, sizeof(push_header_buffer), "PUSH %s/%s %ld\r\n", 
+                                    curr_task->manager_cmd.target_dir,
+                                    curr_task->filename,
+                                    request_bytes);
+
+
+                if(write_all(sock_target_push, push_header_buffer, push_header_len) == -1){
+                    perror("write push");
+                }
+                if(write_all(sock_target_push, pull_buffer, request_bytes) == -1){
+                    perror("write push");
+                }
+
+            } else {
+                if(write_all(sock_target_push, pull_buffer, request_bytes) == -1){
+                    perror("write push");
+                }
+            }
+
+            first_push_write = 1;
+            total_read_pull_req += n_read;
+
+            memset(pull_buffer, 0, sizeof(pull_buffer)); 
+            memset(push_buffer, 0, sizeof(push_buffer)); 
+        }
+        close(sock_target_push);
+    }
+
+    return NULL;
+}
+
+
 // bin/nfs_manager -l logs/manager.log -c config.txt -n 5 -p 2525 -b 10
 int main(int argc, char *argv[]){
     char *logfile       = NULL;
@@ -411,19 +528,12 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    sync_task_ts queue_tasks;
-    if(init_sync_task_ts(&queue_tasks, buffer_size)){
-        perror("init sync task");
-        return 1;
-    }
 
     struct sigaction sa;
     sa.sa_handler = handle_sigint;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0; 
     sigaction(SIGINT, &sa, NULL);
-
-
 
     // Config file parsing
     int fd_config;
@@ -437,107 +547,155 @@ int main(int argc, char *argv[]){
     config_pairs *conf_pairs = malloc(sizeof(config_pairs) * total_config_pairs);
     create_cf_pairs(fd_config, conf_pairs);
     
-    // struct sockaddr_in server, client;
-    // struct sockaddr *serverptr = (struct sockaddr *) &server;
-    // struct sockaddr *clientptr = (struct sockaddr *) &client;
+    sync_task_ts queue_tasks;
+    if(init_sync_task_ts(&queue_tasks, buffer_size)){
+        free(conf_pairs);
 
-    // int socket_manager = 0;
-    // if((socket_manager = socket(AF_INET , SOCK_STREAM , 0)) < 0)
-    //     perror("socket");
+        perror("init sync task");
+        return 1;
+    }
+
+
+    sync_info_mem_store *sync_info_head = NULL;
+    for(int i = 0; i < total_config_pairs; i++){
+        manager_command conf_cmd;
+        conf_cmd.op = ADD;
+
+        strncpy(conf_cmd.source_dir, conf_pairs[i].source_dir_path, BUFFSIZ - 1);
+        conf_cmd.source_dir[BUFFSIZ - 1] = '\0';
+
+        strncpy(conf_cmd.source_ip, conf_pairs[i].source_ip, BUFFSIZ / 4 - 1);
+        conf_cmd.source_ip[BUFFSIZ / 4 - 1] = '\0';
+
+        conf_cmd.source_port = conf_pairs[i].source_port;
+
+        strncpy(conf_cmd.target_dir, conf_pairs[i].target_dir_path, BUFFSIZ - 1);
+        conf_cmd.target_dir[BUFFSIZ - 1] = '\0';
+
+        strncpy(conf_cmd.target_ip, conf_pairs[i].target_ip, BUFFSIZ / 4 - 1);
+        conf_cmd.target_ip[BUFFSIZ / 4 - 1] = '\0';
+
+        conf_cmd.target_port = conf_pairs[i].target_port;
+
+
+        int status = enqueue_add_cmd(conf_cmd, &queue_tasks, &sync_info_head, conf_pairs[i].source_full_path, conf_pairs[i].target_full_path);
+        if(status){
+            perror("enqueue add");
+        }
+    }
+
+    free(conf_pairs);
+    pthread_t worker_th[6];
+    for(int i = 0; i < 6; i++){
+        pthread_create(&worker_th[i], NULL, thread_exec_task, &queue_tasks);
+    }
+    for(int i = 0; i < 6; i++){
+        pthread_join(worker_th[i], NULL);
+    }
+
+
+    print_queue(&queue_tasks);
+
+
+    struct sockaddr_in server, client;
+    struct sockaddr *serverptr = (struct sockaddr *) &server;
+    struct sockaddr *clientptr = (struct sockaddr *) &client;
+
+    int socket_manager = 0;
+    if((socket_manager = socket(AF_INET , SOCK_STREAM , 0)) < 0)
+        perror("socket");
     
-    // int optval = 1;
-    // setsockopt(socket_manager, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    int optval = 1;
+    setsockopt(socket_manager, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
-    // server.sin_family       = AF_INET; /* Internet domain */
-    // server.sin_addr.s_addr  = htonl(INADDR_ANY);
-    // server.sin_port         = htons(port); /* The given port */
+    server.sin_family       = AF_INET; /* Internet domain */
+    server.sin_addr.s_addr  = htonl(INADDR_ANY);
+    server.sin_port         = htons(port); /* The given port */
     
-    // /* Bind socket to address */
-    // if(bind(socket_manager, serverptr, sizeof(server)) < 0){
-    //     perror("bind");
-    //     exit(1);
-    // }
+    /* Bind socket to address */
+    if(bind(socket_manager, serverptr, sizeof(server)) < 0){
+        perror("bind");
+        exit(1);
+    }
 
-    // if(listen(socket_manager, 5) < 0) perror ("listen") ;
+    if(listen(socket_manager, 5) < 0) perror ("listen") ;
     
-    // socklen_t clientlen;
-    // printf("Listening for connections to port % d \n", port);
+    socklen_t clientlen;
+    printf("Listening for connections to port % d \n", port);
     
-    // int socket_console_read = 0;
-    // clientlen = sizeof(struct sockaddr_in);
-    // if((socket_console_read = accept(socket_manager, clientptr, &clientlen)) < 0){
-    //     perror("accept ");
-    //     return 1;
-    // }
+    int socket_console_read = 0;
+    clientlen = sizeof(struct sockaddr_in);
+    if((socket_console_read = accept(socket_manager, clientptr, &clientlen)) < 0){
+        perror("accept ");
+        return 1;
+    }
 
 
-
-    // sync_info_mem_store *sync_info_head = NULL;
-    // printf("Accepted connection \n") ;
-    // while(manager_active){
-    //     char read_buffer[BUFFSIZ];
-    //     memset(read_buffer, 0, sizeof(read_buffer));
+    printf("Accepted connection \n") ;
+    while(manager_active){
+        char read_buffer[BUFFSIZ];
+        memset(read_buffer, 0, sizeof(read_buffer));
         
-    //     ssize_t n_read;
-    //     if((n_read = read(socket_console_read, read_buffer, BUFFSIZ - 1)) <= 0){
-    //         perror("Read from console");
-    //         return 1;
-    //     }
-    //     read_buffer[n_read] = '\0';
+        ssize_t n_read;
+        if((n_read = read(socket_console_read, read_buffer, BUFFSIZ - 1)) <= 0){
+            perror("Read from console");
+            return 1;
+        }
+        read_buffer[n_read] = '\0';
 
 
-    //     char *source_full_path;
-    //     char *target_full_path;
-    //     manager_command curr_cmd;
-    //     if(parse_console_command(read_buffer, &curr_cmd, &source_full_path, &target_full_path)){
-    //         fprintf(stderr, "error: parse_command [%s]\n", read_buffer);
-    //         continue;
-    //     }
+        char *source_full_path;
+        char *target_full_path;
+        manager_command curr_cmd;
+        if(parse_console_command(read_buffer, &curr_cmd, &source_full_path, &target_full_path)){
+            fprintf(stderr, "error: parse_command [%s]\n", read_buffer);
+            continue;
+        }
         
 
-    //     if(curr_cmd.op == ADD){
-    //         int status = enqueue_add_cmd(curr_cmd, &queue_tasks, &sync_info_head, source_full_path, target_full_path);
-    //         if(status){
-    //             perror("enqueue add");
-    //         }
-    //     }
+        if(curr_cmd.op == ADD){
+            int status = enqueue_add_cmd(curr_cmd, &queue_tasks, &sync_info_head, source_full_path, target_full_path);
+            if(status){
+                perror("enqueue add");
+            }
+        }
 
-    //     if(curr_cmd.op == CANCEL){
-    //         int status = enqueue_cancel_cmd(curr_cmd, &queue_tasks, &sync_info_head, source_full_path);
-    //         if(status){
-    //             printf("\n----- Dir not Monitored ---------\n");
-    //             fflush(stdout);
-    //             continue;
-    //         }
-    //     }
+        if(curr_cmd.op == CANCEL){
+            int status = enqueue_cancel_cmd(curr_cmd, &queue_tasks, &sync_info_head, source_full_path);
+            if(status){
+                printf("\n----- Dir not Monitored ---------\n");
+                fflush(stdout);
+                continue;
+            }
+        }
 
-    //     if(curr_cmd.op == SHUTDOWN){
-    //         manager_active = 0;
-    //         continue;
-    //     }
+        if(curr_cmd.op == SHUTDOWN){
+            manager_active = 0;
+            continue;
+        }
 
-    // }
+    }
 
     // print_queue(&queue_tasks);
 
-    // close(socket_console_read);
-    // close(socket_manager);
-
+    
     // printf("\n exiting \n");
     
     ////// Thread workers for tasks
-    pthread_t worker_th[4];
-    for(int i = 0; i < 4; i++){
-        pthread_create(&worker_th[i], NULL, worker_thread, &conf_pairs[i]);
-    }
-    for(int i = 0; i < 4; i++){
-        pthread_join(worker_th[i], NULL);
-    }
+    // pthread_t worker_th[4];
+    // for(int i = 0; i < 4; i++){
+        // pthread_create(&worker_th[i], NULL, worker_thread, &conf_pairs[i]);
+        // }
+        // for(int i = 0; i < 4; i++){
+            //     pthread_join(worker_th[i], NULL);
+            // }
+            
     
-
-    free(conf_pairs);
-    // free_all_sync_info(&sync_info_head);
+    close(socket_manager);
+    close(socket_console_read);
     free(queue_tasks.tasks_array);
+    free_all_sync_info(&sync_info_head);
+
 
     return 0;
 }
